6c46f91bf19d8381e35f62711eda16dc8c644237
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-13 23:12:27 +0300

    update auth view

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,14 @@
+from os import abort
 from flask import render_template, request, redirect, url_for
 from . import main
 from ..request import get_random_quote
 from flask_login import login_required
+from ..models import Comment,Blog,User, PhotoProfile, Random_Quote, Subscription
 
 # Views
 
 
-@main.route('/' )
+@main.route('/', methods = ['GET','POST'] )
 def index():
 
     '''
@@ -16,8 +18,21 @@ def index():
     
     #making api call
     quote=get_random_quote()
+    # blog= get_blogs()
     title = 'Home - diary of a whimpy junior developer'
-    
+
     return render_template('index.html',title=title,quote=quote)
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
 
 
