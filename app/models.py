6d5c511f0cd50c0b23cd8ba26002ebb00d35bc75
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-13 22:16:56 +0300

    test user and comments

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Random_Quote:
     '''
@@ -22,6 +23,19 @@ class User(db.Model):
     profile_pic_path = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id')) 
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
