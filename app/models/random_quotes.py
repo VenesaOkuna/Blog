70a6d8ad11bcaa81b483ab041e8c35ef59490162
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-13 14:56:13 +0300

    add random quote model class

---
+++ app/models/random_quotes.py
@@ -0,0 +1,10 @@
+class Random_Quote:
+    '''
+    Random Qoute class to define random quotes Objects
+    '''
+
+    def __init__(self,id,quote,author):
+        self.id =id
+        self.quote = quote
+        self.author = author
+ 
