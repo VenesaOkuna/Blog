5e3ea57525069d196da964e6698cadd3cae3b275
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-13 21:55:19 +0300

    add user and role classes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Random_Quote:
     '''
     Random Qoute class to define random quotes Objects
@@ -8,3 +10,30 @@ class Random_Quote:
         self.quote = quote
         self.author = author
  
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    blog = db.relationship('Blog', backref = 'user', lazy='dynamic')
+    all_comments = db.relationship('Comment',backref = 'user',lazy='dynamic')
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id')) 
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
